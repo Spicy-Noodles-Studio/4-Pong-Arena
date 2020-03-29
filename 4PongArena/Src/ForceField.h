71908bef882da42b0a91369638013cb0432b31f0
Martin Amechazurra <mamechaz@ucm.es>
2020-03-29 22:23:33 +0200

    Created ForceField component
    Creada la clase ForceField con la funcionalidad
    
    Creado blueprint de ForceField

---
+++ 4PongArena/Src/ForceField.h
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef FORCE_FIELD_H
+#define FORCE_FIELD_H
+
+#include <UserComponent.h>
+#include <GameObject.h>
+#include <RigidBody.h>
+
+class ForceField : public UserComponent
+{
+private:
+
+	RigidBody* rigidBody;
+
+	float force;
+	float stateTime;
+
+	bool random;
+
+	enum state {DISABLED, FORWARDS, BACKWARDS};
+
+	state currentState;
+	float currentTime;
+
+	//virtual void onTriggerEnter(GameObject* other);
+	virtual void onObjectStay(GameObject* other);
+
+public:
+	ForceField(GameObject* gameObject);
+
+	virtual void start();
+	virtual void update(float deltaTime);
+	virtual void handleData(ComponentData* data);
+
+	void setState(std::string state);
+	void setForce(float _force) { force = _force; }
+	void setTime(float time) { stateTime = time; }
+	void setRandom(bool _random) { random = _random; }
+};
+
+#endif
