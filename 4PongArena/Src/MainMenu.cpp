2e52fd823e7219ea71331f1130addbbf8cab9b96
xRyusey <xRyusey@users.noreply.github.com>
2020-04-14 21:14:02 +0200

    Menu de configuración funcional y menú principal

---
+++ 4PongArena/Src/MainMenu.cpp
@@ -0,0 +1,54 @@
+#include "MainMenu.h"
+#include <InterfaceSystem.h>
+#include <SceneManager.h>
+#include <GameObject.h>
+
+#include "GameManager.h"
+
+#include <ComponentRegister.h>
+
+REGISTER_FACTORY(MainMenu);
+
+bool MainMenu::singlePlayerButtonClick()
+{
+	SceneManager::GetInstance()->changeScene("ConfigurationMenu");
+	return false;
+}
+
+bool MainMenu::multiplayerButtonClick()
+{
+	SceneManager::GetInstance()->changeScene("ConfigurationMenu");
+	return false;
+}
+
+bool MainMenu::optionsButtonClick()
+{
+	SceneManager::GetInstance()->changeScene("OptionsMenu");
+	return false;
+}
+
+bool MainMenu::exitButtonClick()
+{
+
+	return false;
+}
+
+MainMenu::MainMenu(GameObject* gameObject) : UserComponent(gameObject)
+{
+	InterfaceSystem* interfaceSystem = InterfaceSystem::GetInstance();
+
+	interfaceSystem->registerEvent("singlePlayerButtonClick", UIEvent("ButtonClicked", [this]() {return singlePlayerButtonClick(); }));
+	interfaceSystem->registerEvent("multiplayerButtonClick", UIEvent("ButtonClicked", [this]() {return multiplayerButtonClick(); }));
+	interfaceSystem->registerEvent("optionsButtonClick", UIEvent("ButtonClicked", [this]() {return optionsButtonClick(); }));
+	interfaceSystem->registerEvent("exitButtonClick", UIEvent("ButtonClicked", [this]() {return exitButtonClick(); }));
+}
+
+MainMenu::~MainMenu()
+{
+	InterfaceSystem* interfaceSystem = InterfaceSystem::GetInstance();
+
+	interfaceSystem->unregisterEvent("singlePlayerButtonClick");
+	interfaceSystem->unregisterEvent("multiplayerButtonClick");
+	interfaceSystem->unregisterEvent("optionsButtonClick");
+	interfaceSystem->unregisterEvent("exitButtonClick");
+}
