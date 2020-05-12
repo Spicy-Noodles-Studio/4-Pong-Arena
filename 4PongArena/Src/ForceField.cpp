6cb13f7980bf63a78b5b3c2a89ee511582105893
argarc03 <argarc03@ucm.es>
2020-05-12 17:04:42 +0200

    ForceField acelera menos y las bolas no deberían de atravesar escenario

--- 4PongArena/Src/ForceField.cpp
+++ 4PongArena/Src/ForceField.cpp
@@ -1,7 +1,6 @@
 #include "ForceField.h"
 #include <GameObject.h>
 #include <RigidBody.h>
-#include <MathUtils.h>
 #include <sstream>
 
 #include "Ball.h"
@@ -10,31 +9,14 @@
 
 REGISTER_FACTORY(ForceField);
 
-ForceField::ForceField(GameObject* gameObject) :	UserComponent(gameObject), targetVelocity(0.0f), acceleration(0.0f), stateTime(0.0f), stateTimer(0.0f), random(false), currentState(State::FORWARDS)
+ForceField::ForceField(GameObject* gameObject) : UserComponent(gameObject), targetVelocity(0.0f), acceleration(0.0f)
 {
-	
+
 }
 
 ForceField::~ForceField()
 {
 
-}
-
-void ForceField::start()
-{
-	currentState = State::FORWARDS;
-	stateTimer = 0.0f;
-}
-	
-void ForceField::update(float deltaTime)
-{
-	/*stateTimer += deltaTime;
-
-	if (stateTimer >= stateTime)
-	{
-		changeState();
-		stateTimer = 0;
-	}*/
 }
 
 void ForceField::handleData(ComponentData* data)
@@ -53,16 +35,6 @@ void ForceField::handleData(ComponentData* data)
 			if (!(ss >> acceleration))
 				LOG("FORCE FIELD: Invalid value for property with name \"%s\"", prop.first.c_str());
 		}
-		else if (prop.first == "stateTime")
-		{
-			if (!(ss >> stateTime))
-				LOG("FORCE FIELD: Invalid value for property with name \"%s\"", prop.first.c_str());
-		}
-		else if (prop.first == "random")
-		{
-			if (!(ss >> random))
-				LOG("FORCE FIELD: Invalid value for property with name \"%s\"", prop.first.c_str());
-		}
 		else
 			LOG("FORCE FIELD: Invalid property with name \"%s\"", prop.first.c_str());
 	}
@@ -70,32 +42,13 @@ void ForceField::handleData(ComponentData* data)
 
 void ForceField::onObjectEnter(GameObject* other)
 {
-	if (currentState == State::DISABLED) return;
-
 	RigidBody* rigidBody = other->getComponent<RigidBody>();
 	Ball* ball = other->getComponent<Ball>();
 
 	if (ball == nullptr || rigidBody == nullptr) return;
-	/*
-	if (currentState != State::FORWARDS)
-		rigidBody->setLinearVelocity(rigidBody->getLinearVelocity() * -1);
-	*/
+
 	ball->setTargetVelocity(targetVelocity);
 	ball->setAcceleration(acceleration);
-}
-
-/// States: "DISABLED" | "FORWARDS" | "BACKWARDS"
-void ForceField::setState(const std::string& state)
-{
-	if (state == "DISABLED") currentState = State::DISABLED;
-	else if (state == "FORWARDS") currentState = State::FORWARDS;
-	else if(state == "BACKWARDS") currentState = State::BACKWARDS;
-	else currentState = State::DISABLED;
-}
-
-void ForceField::setState(State state)
-{
-	currentState = state;
 }
 
 void ForceField::setTargetVelocity(float targetVelocity)
@@ -106,48 +59,4 @@ void ForceField::setTargetVelocity(float targetVelocity)
 void ForceField::setAcceleration(float acceleration)
 {
 	this->acceleration = acceleration;
-}
-
-void ForceField::setTime(float time)
-{
-	stateTime = time;
-}
-
-void ForceField::setRandom(bool random)
-{
-	this->random = random;
-}
-
-void ForceField::changeState()
-{
-	if (random) {
-		int rnd = rand() % 3;
-		switch (rnd) {
-		case 0:
-			currentState = State::FORWARDS;
-			break;
-		case 1:
-			currentState = State::BACKWARDS;
-			break;
-		case 2:
-			currentState = State::DISABLED;
-			break;
-		default:
-			currentState = State::DISABLED;
-			break;
-		}
-	}
-	else {
-		switch (currentState) {
-		case State::DISABLED:
-			currentState = State::FORWARDS;
-			break;
-		case State::FORWARDS:
-			currentState = State::BACKWARDS;
-			break;
-		case State::BACKWARDS:
-			currentState = State::DISABLED;
-			break;
-		}
-	}
 }
--- 4PongArena/Src/ForceField.h
+++ 4PongArena/Src/ForceField.h
@@ -4,49 +4,22 @@
 
 #include <UserComponent.h>
 
-class RigidBody;
-
 class ForceField : public UserComponent
 {
-public:
-	enum class State { DISABLED, FORWARDS, BACKWARDS };
-
 private:
 	// Properties given to the ball
 	float targetVelocity;
 	float acceleration;
 
-	// Time to change state
-	float stateTime;
-
-	// Timer
-	float stateTimer;
-
-	// Boolean so State change is or is not random
-	bool random;
-
-	// Current ForceField state
-	State currentState;
-
 public:
 	ForceField(GameObject* gameObject);
 	virtual ~ForceField();
 
-	virtual void start();
-	virtual void update(float deltaTime);
 	virtual void handleData(ComponentData* data);
 	virtual void onObjectEnter(GameObject* other);
 
-	void setState(const std::string& state);
-	void setState(State state);
-
 	void setTargetVelocity(float targetVelocity);
 	void setAcceleration(float acceleration);
-	void setTime(float time);
-	void setRandom(bool random);
-
-private:
-	void changeState();
 };
 
 #endif
